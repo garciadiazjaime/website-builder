2e507e21353c08895d61cfcbeec7f9d5df7423fa
alex <alejandro@mintitmedia.com>
2015-12-17 18:23:10 -0800

    nosotros parcial

--- src/components/sections/aboutus/teamMembers.js
+++ src/components/sections/aboutus/teamMembers.js
@@ -1,9 +1,10 @@
 import React, { Component, PropTypes} from 'react';
 
+import Sprites from '../../../constants/sprite';
 import Colors from '../../../constants/colors';
-import Sprites from '../../../sprites/sprite';
 import Fonts from '../../../constants/fonts';
-import CardX from '../../widgets/card/CardX';
+import CardX from '../../widgets/card/cardX';
+
 
 export default class OurTeam extends Component {
 
@@ -12,12 +13,9 @@ export default class OurTeam extends Component {
     this.state = {
       style: {
         wrapper: {
-          background: "url('http://127.0.0.1:3000/images/ourteam-bg.png') no-repeat center bottom",
-          backgroundSize: '100% auto',
-          marginTop: '-150px',
-          paddingTop: '250px',
+          background: Colors.yellow,
           position: 'relative',
-          zIndex: '-1'
+          padding: '100px 0'
         }
       }
     };
@@ -25,40 +23,60 @@ export default class OurTeam extends Component {
 
 
   render() {
-    var data = {
-      cardA: {
-        wrapper: {
-          style: {
-            
-          }
-        },
-        cards:[{
-          type: 'title',
-          text: ['Nuestro Equipo'],
-          style: _.merge({}, Fonts.typeE,{
-            margin: '0 0 30px'
-          })
-        },
-        {
-          type: 'description',
-          text: ['Nuestro Equipo de profesionales, estan capacitados para atenderte de forma rápida y eficiente, contacta a uno de nuestros asesores:'],
-          style: _.merge({}, Fonts.typeA)
-        }]
-      }
-    }
+    var cards = this.getCards();
    
     return (
       <div style={this.state.style.wrapper}>
         <div className="container">
           <div className="row">
             <div className="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">
-              <div>
-                <CardX data={data}/>
-              </div>
+              <CardX data={cards.cardA} />
+            </div>
+            <div className="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">
+              <CardX data={cards.cardA} />
+            </div>
+            <div className="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">
+              <CardX data={cards.cardA} />
+            </div>
+            <div className="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">
+              <CardX data={cards.cardA} />
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  getCards() {
+    return {
+      cardA: {
+        wrapper: {
+          style: {
+            padding: '35px 40px 50px',
+            textAlign: 'center'
+          }
+        },
+        elements: [{
+          type: 'title',
+          text: ['Nombre Apellido'],
+          style: {
+            colors: Colors.red
+          }
+        },
+        {
+          type: 'subtitle',
+          text: ['Puesto'],
+          style: {
+            colors: Colors.red
+          }
+        }, {
+          type: 'description',
+          text: ['998-0009-33', 'mail@dominio.com'],
+          style: {
+            colors: Colors.red
+          }
+        }]
+      }
+    };
   }
 }
--- src/components/widgets/card/cardX/index.js
+++ src/components/widgets/card/cardX/index.js
@@ -13,16 +13,75 @@ export default class CardX extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      style: this.getStyle('INIT')
     };
   }
 
+  componentDidMount() {
+    const utilRD = new UtilRD();
+
+    if (utilRD.isTablet(document.body.clientWidth)) {
+      this.setState({
+        style: _.merge({}, this.state.style, this.getStyle('TABLET'))
+      });
+    }
+    else if (utilRD.isPhone(document.body.clientWidth)) {
+      this.setState({
+        style: _.merge({}, this.state.style, this.getStyle('PHONE'))
+      });
+    }
+  }
+
   render() {
+    var style = this.getStyle();
 
     return (
-      <div>
-        adios
+      <div style={style.wrapperStyle}>
+        <SimpleCard data={this.props.data} />
       </div>
     );
+  }
+
+  getStyle(type) {
+    switch (type) {
+      case 'INIT':
+        return {
+          wrapperStyle: {
+            background: 'white',
+            margin: '0 -11px'
+          }
+        };
+        break;
+      case 'TABLET':
+        return {
+          wrapperStyle: _.merge({}, this.state.style.wrapperStyle, {
+            paddingLeft: '25px',
+            paddingTop: '70px',
+          })
+        };
+      case 'PHONE':
+        return {
+          wrapperStyle: _.merge({}, this.state.style.wrapperStyle, {
+            paddingTop: '25px'
+          })
+        };
+      default:
+        return {
+          titleStyle: {
+            fontFamily: 'noto-sans-bold',
+            fontSize: '40px',
+            color: Colors.blueDark,
+            marginBottom: '50px'
+          },
+          textRed: {
+            color: Colors.red
+          },
+          wrapperStyle: {
+            background: 'white',
+            margin: '0 -11px'
+          }
+        };
+    }
   }
 }
 
